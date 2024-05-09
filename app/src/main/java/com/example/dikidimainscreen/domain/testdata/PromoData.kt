7c0e2140e67054870c8070543844ab9cf0c2aaba
Роман Савельев <r.zrk@yandex.ru>
2024-05-09 19:29:59 +0500

    Create Block Promo

---
+++ app/src/main/java/com/example/dikidimainscreen/domain/testdata/PromoData.kt
@@ -0,0 +1,8 @@
+package com.example.dikidimainscreen.domain.testdata
+
+data class PromoData(
+    val imageId: Int,
+    val companyName: String,
+    val promoDescription: String,
+    val address: String
+)
